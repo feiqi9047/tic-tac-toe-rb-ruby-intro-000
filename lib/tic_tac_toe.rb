0fb13f9d8fd2d3abe1b3245c4d6a4fe24c8dde97
Fei Qi <feiqi9047@gmail.com>
2018-11-27 23:17:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,7 @@
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 board = [" ", " ", " ", " ", " ", " "," ", " ", " "]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -72,6 +73,7 @@ def won?(board)
     |combo| 
     if combo.all? {|index| board[index]=="X"} || combo.all? {|index| board[index]=="O"}
   end
+end
 end
 
 def full(board)
